3f6d5eca971ab02638c568c65cd999538b0e1a59
westorres9 <wester.torres9@gmail.com>
2022-03-19 07:53:16 -0300

    created initial project hrworker

---
+++ humanResourceWorker/src/main/java/com/devsuperior/humanResourceWorker/HumanResourceWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.humanResourceWorker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceWorkerApplication.class, args);
+	}
+
+}
---
+++ humanResourceWorker/src/test/java/com/devsuperior/humanResourceWorker/HumanResourceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.humanResourceWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
