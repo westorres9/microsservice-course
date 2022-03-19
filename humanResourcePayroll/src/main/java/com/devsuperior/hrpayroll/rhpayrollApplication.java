02655f280c69bd4d549dd50ef212c3e7bc092931
westorres9 <wester.torres9@gmail.com>
2022-03-19 10:16:41 -0300

    RestTemplate

--- humanResourcePayroll/src/main/java/com/devsuperior/hrpayroll/rhpayrollApplication.java
+++ humanResourcePayroll/src/main/java/com/devsuperior/hrpayroll/rhpayrollApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HumanResourcePayrollApplication {
+public class rhpayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HumanResourcePayrollApplication.class, args);
+		SpringApplication.run(rhpayrollApplication.class, args);
 	}
 
 }
