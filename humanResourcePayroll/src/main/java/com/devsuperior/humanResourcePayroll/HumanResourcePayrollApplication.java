96dd200980d099aa310e9e6289d64c8ac9456af3
westorres9 <wester.torres9@gmail.com>
2022-03-19 09:02:29 -0300

    project hrPayroll created and retrieve to version 2.3.x

---
+++ humanResourcePayroll/src/main/java/com/devsuperior/humanResourcePayroll/HumanResourcePayrollApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.humanResourcePayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcePayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcePayrollApplication.class, args);
+	}
+
+}
